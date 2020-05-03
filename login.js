2ce0b71dcc776f10d0cc7d40fd1b39de243d117d
André <40955428+gh0st56@users.noreply.github.com>
2020-05-03 12:01:56 -0300

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function Login(){
         var valid = "WyJkIiwiZiIsInYiLCJhIiwiYSIsImZnIiwibWluIl0=";
         var decod = atob(valid);
         var names = eval(decod);
-        var name = prompt("Enter your name", "");
+        var name = prompt("Write your name here | Ecrivez votre nom ici", "");
         if(name == names[3]+names[0]+names[6]){
                 alert("Welcome "+names[3]+names[0]+names[6]);
         }
